ccadbdd59a4dd6bb6ce97aeaaa2a8881a165c288
Alexandru-Cojocari <144117735+Alexandru-Cojocari@users.noreply.github.com>
2023-11-14 14:10:40 +0200

    Add files via upload

---
+++ Exercitiu 8 Cojocari Alexandru 2023 14.11.py
@@ -0,0 +1,8 @@
+n = int(input('nr de zile='))
+capete_balaur = 6
+for zi in range(n):
+    capete_balaur -= 1
+    capete_balaur += 6
+
+
+print('Numărul de capete al balaurului după', n ,'zile este:', capete_balaur)
